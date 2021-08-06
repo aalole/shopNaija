960650f8848722e6eb5720ce62d595fb3832d335
aalole <abiodunrasheed93@gmail.com>
2021-08-06 10:24:26 -0700

    implemented redux for product resources state management

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from "react";
-// import products from "../products";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import { Row, Col } from "react-bootstrap";
-import axios from "axios";
 
 const Homescreen = () => {
-  const [products, setProducts] = useState([]);
-
+  const dispatch = useDispatch();
+  const productList = useSelector((s) => s.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1 className='text-center py-2'> Latest products </h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm='12' md='6' lg='4'>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'> {error} </Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm='12' md='6' lg='4'>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
